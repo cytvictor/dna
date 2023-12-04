a6c8572a34278d51d06c127a05a677110a7dfca3
Yongting <yongting@nyu.edu>
2023-12-04 13:53:28 +0000

    fix for DynamicDijkstra

--- projects/dp-generator/src/main/java/org/ants/generator/routing/Network.java
+++ projects/dp-generator/src/main/java/org/ants/generator/routing/Network.java
@@ -8,8 +8,10 @@ import java.util.Map;
 import java.util.Set;
 
 import org.ants.generator.algo.DijkstraMultiPath;
+import org.ants.generator.algo.DynamicDijkstra;
 import org.ants.generator.algo.Graph;
 import org.ants.generator.algo.Path;
+import org.ants.generator.algo.TWeight;
 import org.ants.generator.routing.path.BGPPath;
 import org.ants.generator.routing.weight.ComparableBGPWeight;
 import org.ants.parser.datamodel.Prefix;
@@ -19,7 +21,7 @@ import org.ants.parser.relation.neighbor.*;
 public class Network {
 
   // private DiGraph<Node> oldGraph;
-  private Graph<Node, ComparableBGPWeight> graph;
+  private Graph<Node> graph;
   private ArrayList<BgpNetwork> networksPendingPropagate;
 
   public Network() {
@@ -49,8 +51,8 @@ public class Network {
           Node node2 = bgpNeighbor.node2;
           // oldGraph.addEdge(node1, node2);
           // oldGraph.addEdge(node2, node1);
-          graph.addEdge(node1, node2, new ComparableBGPWeight(false));
-          graph.addEdge(node2, node1, new ComparableBGPWeight(false));
+          graph.addEdge(node1, node2, new TWeight(0));
+          graph.addEdge(node2, node1, new TWeight(0));
         }
       }
     }
@@ -68,33 +70,33 @@ public class Network {
       System.out.println("Propagating " + bgpNetwork);
 
       // 1. Find SPT for this bgpNetwork
-      DijkstraMultiPath<Node, ComparableBGPWeight> dijkstra = new DijkstraMultiPath<>();
-      Map<Node, List<Path<Node, ComparableBGPWeight>>> shortestPaths = dijkstra.findShortestPaths(graph,
-          bgpNetwork.node, new BGPPath<Node, ComparableBGPWeight>(bgpNetwork.node, bgpNetwork));
+      DynamicDijkstra<Node> dijkstra = new DynamicDijkstra<>();
+      Map<Node, Path<Node, TWeight>> shortestPaths = dijkstra.findShortestPaths(graph,
+          bgpNetwork.node, new BGPPath<Node, TWeight>(bgpNetwork.node, bgpNetwork));
 
       // 2. Derive RIBs from SPT
-      for (Map.Entry<Node, List<Path<Node, ComparableBGPWeight>>> entry : shortestPaths.entrySet()) {
-        System.out.println("Shortest paths to " + entry.getKey().node + ":");
-        for (Path<Node, ComparableBGPWeight> path : entry.getValue()) {
-          System.out.println("  " + path);
-          Node nextHop = null;
-          for (Node visitedNode : path.getVertices()) {
-            Set<BgpRib> ribs = nodeRibs.get(visitedNode);
-            if (ribs == null) {
-              ribs = new HashSet<>();
-              nodeRibs.put(visitedNode, ribs);
-            }
-            if (visitedNode == bgpNetwork.node) {
-              // 如果是本地节点，那么就是local rib, 下一跳是本地
-              ribs.add(new BgpRib(((BGPPath<Node, ComparableBGPWeight>) path).getBgpNetwork().prefix, bgpNetwork.node,
-                  true));
-            } else {
-              // 如果是其他节点，那么就是adj rib, 下一跳是 SPT 前一个节点
-              ribs.add(new BgpRib(((BGPPath<Node, ComparableBGPWeight>) path).getBgpNetwork().prefix, nextHop));
-            }
-            nextHop = visitedNode;
+      for (Map.Entry<Node, Path<Node, TWeight>> path : shortestPaths.entrySet()) {
+        // System.out.println("Shortest paths to " + entry.getKey().node + ":");
+        // for (Path<Node, TWeight> path : entry.getValue()) {
+        System.out.println("  " + path);
+        Node nextHop = null;
+        for (Node visitedNode : path.getValue().getVertices()) {
+          Set<BgpRib> ribs = nodeRibs.get(visitedNode);
+          if (ribs == null) {
+            ribs = new HashSet<>();
+            nodeRibs.put(visitedNode, ribs);
           }
+          if (visitedNode == bgpNetwork.node) {
+            // 如果是本地节点，那么就是local rib, 下一跳是本地
+            ribs.add(new BgpRib(((BGPPath<Node, TWeight>) path.getValue()).getBgpNetwork().prefix, bgpNetwork.node,
+                true));
+          } else {
+            // 如果是其他节点，那么就是adj rib, 下一跳是 SPT 前一个节点
+            ribs.add(new BgpRib(((BGPPath<Node, TWeight>) path.getValue()).getBgpNetwork().prefix, nextHop));
+          }
+          nextHop = visitedNode;
         }
+        // }
       }
     }
 
